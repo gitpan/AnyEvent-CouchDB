7b18facce30a98b2f82f7b669251d41f5fdf500d
John Beppu <beppu@cpan.org>
2013-06-06 05:37:36 +0000

    Import of BEPPU/AnyEvent-CouchDB-1.31 from CPAN.
    gitpan-cpan-distribution: AnyEvent-CouchDB
    gitpan-cpan-version:      1.31
    gitpan-cpan-path:         BEPPU/AnyEvent-CouchDB-1.31.tar.gz
    gitpan-cpan-author:       BEPPU
    gitpan-cpan-maturity:     released

--- lib/AnyEvent/CouchDB.pm
+++ lib/AnyEvent/CouchDB.pm
@@ -2,7 +2,7 @@ package AnyEvent::CouchDB;
 
 use strict;
 use warnings;
-our $VERSION = '1.30';
+our $VERSION = '1.31';
 
 use JSON;
 use AnyEvent::HTTP;
@@ -23,7 +23,7 @@ our $HTTPError = "AnyEvent::CouchDB::Exception::HTTPError";
 our $JSONError = "AnyEvent::CouchDB::Exception::JSONError";
 
 # default JSON encoder
-our $default_json = JSON->new->utf8;
+our $default_json = JSON->new->allow_nonref->utf8;
 
 # arbitrary uri support
 sub _build_headers {
@@ -529,7 +529,9 @@ James Howe (bug reports)
 
 Dave Williams (bug fixes)
 
-Walter Werner (buf fixes)
+Walter Werner (bug fixes)
+
+Maroun NAJM (bulk doc enhancements)
 
 =head1 COPYRIGHT
 
